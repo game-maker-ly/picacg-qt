fe73ce28a738df27fe9c07b8daad3001516f455e
game-maker-ly <59645562+game-maker-ly@users.noreply.github.com>
2024-01-30 15:01:17 +0800

    Update start.py
    修复a卡闪退

--- src/start.py
+++ src/start.py
@@ -29,6 +29,8 @@ except Exception as es:
     if hasattr(es, "msg"):
         config.ErrorMsg = es.msg
 
+config.CanWaifu2x = False
+
 from PySide6.QtGui import QFont, QFontDatabase
 from PySide6 import QtWidgets  # 导入PySide6部件
 from PySide6.QtNetwork import QLocalSocket, QLocalServer
